9cc8ef816b61f1eb7935c38e124f78e8df7672ad
shakilmahammud <hshakil987@gmail.com>
2020-07-18 12:13:47 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-state-counter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-state-counter)
---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class Counter extends React.Component {
+  state = {
+    counter: 0
+  }
+  increment = () => {
+    this.setState({counter: this.state.counter+1});
+  }
+  render() {
+    return <div>
+    <p>{this.state.counter}</p>
+    <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+}
+
+const el = <Counter />; 
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+);
